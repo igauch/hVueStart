97eaa8f9f26b0080f92ca3063bd308612c4011ae
igauch <twj940525@gmail.com>
2017-10-27 15:39:30 +0800

    gauch

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -1,19 +1,26 @@
+/*
+* 描述：hello
+* 作者：twj94
+* 创建时间：2017/10/27
+* 版权：Copyright 2017 Howso Tech. Co. Ltd. All Rights Reserved.
+* Company:南京华苏科技有限公司
+*/
+
 <template>
-    <div class="hello">
-        <img src="../styles/imgs/logo.png" alt="华苏" class="mx-auto">
-        <h1>{{ msg }}</h1>
-    </div>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'hello',
-        data () {
-            return {
-                msg: '欢迎使用华苏VUE构建工具'
-            }
-        }
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: '欢迎使用华苏VUE构建工具'
+      }
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ template/src/components/topbar.vue
@@ -0,0 +1,39 @@
+/*
+* 描述：topbar
+* 作者：twj94
+* 创建时间：2017/10/27
+* 版权：Copyright 2017 Howso Tech. Co. Ltd. All Rights Reserved.
+* Company:南京华苏科技有限公司
+*/
+
+<template>
+  <div class="text-white top-bar bg-primary">
+    <div class="container-fluid">
+      <router-link :to="{path: '/'}">
+        <img src="../styles/imgs/logo.png" alt="logo" class="logo align-middle">
+        <span class="font-weight-bold align-middle align-middle logo-text">demo</span>
+      </router-link>
+      <div class="float-right user-info">
+        <span>欢迎你，{{userName}}</span>
+        <i class="iconfont icon-loginout mx-3" @click="logout"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'topBar',
+    data() {
+      return {
+        userName: sessionStorage.getItem('userName')
+      }
+    },
+    methods: {
+      logout: function () {
+        sessionStorage.setItem('userName', null);
+        this.$router.push('/login');
+      }
+    }
+  }
+</script>
